4810fd3532d0164f6c9a5fafe3c965d0016579c0
Joan-Stiven <joanstivensilva12@gmail.com>
2024-09-28 18:41:29 -0500

    Casi terminado

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggler = document.querySelector('.navbar-toggler');
-    const navbarNav = document.querySelector('#navbarNav');
-
-    toggler.addEventListener('click', function () {
-        navbarNav.classList.toggle('show');
-    });
-});
